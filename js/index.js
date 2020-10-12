1fec63d2bdb2e9e6c5b4780520c69e1829dde697
Tsung-Wei Huang <twh760812@gmail.com>
2020-10-11 21:50:18 -0600

    updated javascript to read news

---
+++ js/index.js
@@ -0,0 +1,55 @@
+// Author: twhuang
+'use strict';
+
+function makeCard(item) {
+  if(item == null || item == undefined) return ''; 
+  return `\
+<div class="card">\
+<div class="card-body">\
+<p><a href="${item.image_url}"><img class="card-img-top" src="${item.image_src}" alt="iccad"></a></p>\
+<h5 class="card-title">${item.title}</h5>\
+<p class="card-text">${item.content}</p>\
+</div>\
+<div class="card-footer">\
+<small class="text-muted">${item.footer}</small>\
+</div>\
+</div>`
+}
+
+let row = 0;
+let cnt = 0;
+
+// first three rows are explicit
+for(row=1; row<=3; row=row+1, cnt+=3) {
+  let news_div = $(`#tag_news > div:nth-child(${row})`);
+  news_div.after(`<div class="card-deck mb-4" id=news_row_${row}></div>`)
+  $(`#news_row_${row}`).append(
+    makeCard(news[cnt]), makeCard(news[cnt+1]), makeCard(news[cnt+2])
+  )
+}
+
+// the rest rows are implicit
+for(; cnt<news.length; cnt+=3, row++) {
+  let news_div = $(`#tag_news > div:nth-child(${row})`);
+  news_div.after(`<div class="card-deck hideNews mb-4" id=news_row_${row}></div>`)
+  $(`#news_row_${row}`).append(
+    makeCard(news[cnt]), makeCard(news[cnt+1]), makeCard(news[cnt+2])
+  )
+}
+
+// more news
+$('#moreNews').on('click', function(e) {
+  e.preventDefault();
+  let length = $('.hideNews').length;
+  if(length > 0) {
+    let button_height = $('#moreNews').height();
+    let news = $('.hideNews').slice(0, 1);
+    news.removeClass('hideNews');
+    $('html, body').animate({
+      scrollTop: news.position().top - 50
+    }, 100);
+    //$('.conv ').animate({
+    //  scrollTop: $('.div .hideNews:last-child').position().top
+    //}, 'slow');
+  }
+});
---
+++ js/news.js
@@ -0,0 +1,87 @@
+const news = [
+  {
+    title: "Taskflow @ ICCAD 20",
+    image_src: "images/2020_ICCAD-logo.jpg",
+    image_url: "https://iccad.com/",
+    content: "We will share our experience of using Taskflow to parallelize CAD in the tutorial session <q><a href=\"https://iccad.com/event_details?id=305-12-D\">GPU Acceleration in CAD: Opportunities and Challenges</a></q> at the 2020 IEEE/ACM International Conference on Computer-aided Design (ICCAD)!",
+    footer: "Last updated on 2020/10/11"
+  },
+  {
+    title: "Taskflow @ WOSET 20",
+    image_src: "images/woset20.png",
+    image_url: "https://woset-workshop.github.io/",
+    content: "We are presenting Taskflow at the 2020 Workshop of Open-source EDA Technology (WOSET) co-located with the International Conference on Computer-aided Design (ICCAD)!",
+    footer: "Last updated on 2020/10/11"
+  },
+  {
+    title: "Taskflow @ MUC++ 2020",
+    image_src: "images/mucpp-meetup.jpeg",
+    image_url: "https://www.meetup.com/MUCplusplus/events/273223910/",
+    content: `We are presenting Taskflow to the C++ group at München, Germany. Please join the event, <q><a href="https://www.meetup.com/MUCplusplus/events/273223910/">Taskflow: A General-purpose Parallel and Heterogeneous Task Programming System</a></q>, on Thu, Oct 22, 7:00 PM GMT+2.`,
+    footer: "Last updated on 2020/09/25"
+  },
+  {
+    title: "Taskflow @ CIE/USA-GNYC 2020",
+    image_src: "images/CIE-USA-GNYC-20.jpg",
+    image_url: "https://www.cieusa-gnyc.org/new2019/2020-convention/",
+    content: `We are presenting Taskflow at the CIE/USA-GNYU Annual Convention (virtual conference). Please join the event, <q><a href="https://www.cieusa-gnyc.org/new2019/2020-convention-tsung-wei-huang/">A General-purpose Parallel and Heterogeneous Task Programming System at Scale</a></q>, on Sunday, Oct 18, 2:20-3:50 PM EST.`,
+    footer: "Last updated on 2020/09/21"
+  },
+  {
+    title: "Taskflow @ Barcelona C++ 2020",
+    image_src: "images/barcelona-cppmeetup.jpeg",
+    image_url: "https://www.meetup.com/C-Programmer-Meetup/events/273434145/",
+    content: `We are presenting Taskflow to the C++ group at Barcelona, Spain. Please join the event, <q><a href="https://www.meetup.com/C-Programmer-Meetup/events/273434145/">Taskflow: Parallel and Heterogeneous Task Programming in C++</a></q>, on Wed, Oct 14, 7:00 PM GMT+2.`,
+    footer: "Last updated on 2020/09/20"
+  },
+  {
+    title: "Taskflow @ Cpp-India 2020",
+    image_src: "images/cpp-india-20.jpg",
+    image_url: "https://www.cppindia.co.in/",
+    content: `We are presenting Taskflow to the C++ group in India (Cpp-India). Please join the event, <q><a href="https://www.cppindia.co.in/TechTalk/2020/">Parallel Programming in Modern C++</a></q>, on Sat, 11:30 AM, IST.`,
+    footer: "Last updated on 2020/09/21"
+  },
+  {
+    title: "Taskflow won the Champion of 2020 HPEC Graph Challenge",
+    image_src: "images/hpec-gc20.png",
+    image_url: "http://graphchallenge.mit.edu/champions",
+    content: `Our Taskflow-powered machine learning algorithm, <q><i>A Novel Inference Algorithm for Large Sparse Neural Network using Task Graph Parallelism,</i></q> is placed on the <a href="http://graphchallenge.mit.edu/champions">Champions</a> of the 2020 IEEE/MIT/Amazon HPEC Graph Challenge.`,
+    footer: "Last updated on 2020/08/25"
+  },
+  {
+    title: "Taskflow @ CppCon 2020",
+    image_src: "images/cppcon.png",
+    image_url: "https://cppcon.org/",
+    content: `We are glad Taskflow is accepted by the official 2020 C++ conference as a regular presentation! The presentation title is <q><i>Taskflow: A General-purpose Parallel and Heterogeneous Task Programming System using Modern C++</i></q>.`,
+    footer: "Last updated on 2020/08/12"
+  },
+  {
+    title: "Taskflow @ VLSI-DAT 2020",
+    image_src: "images/vlsidat-logo.png",
+    image_url: "https://expo.itri.org.tw/2020VLSIDAT",
+    content: `We gave an invited talk, <q><i>Programming Systems for Parallelizing VLSI CAD and Beyond</i></q>, at the special session of the 2020 VLSI-DAT symposium.`,
+    footer: "Last updated on 2020/08/01"
+  },
+  {
+    title: "Taskflow @ ORNL 2020",
+    image_src: "images/ornl.png",
+    image_url: "https://www.ornl.gov/group/ft",
+    content: `We visited the Future Technology Group at Oak Ridge National Lab and presented Taskflow to ORNL researchers. The presentation slide is available <a href="https://tsung-wei-huang.github.io/talk/ornl-20.pdf">here</a>.`,
+    footer: "Last updated on 2020/05/01"
+  },
+  {
+    title: "Taskflow Won Software Award @ ACM MM 2019",
+    image_src: "images/2019_ACMMM-logo.png",
+    image_url: "https://2019.acmmm.org/",
+    content: `Taskflow received the Second Prize in the <i>Open Source Software Competition</i> at the 2019 ACM Multimedia Conference! We are grateful for all the effort made by  users and contributors.`,
+    footer: "Last updated on 2019/10/30"
+  },
+  {
+    title: "Taskflow @ IPDPS 2019",
+    image_src: "images/ipdps-2019-logo.jpg",
+    image_url: "https://www.ipdps.org/ipdps2019/2019-advance-program.html",
+    content: `We presented Taskflow at the 2019 IEEE International Parallel and Distributed Processing Symposium (Session 23: Programming Languages).`,
+    footer: "Last updated on 2019/06/01"
+  }
+]
+
