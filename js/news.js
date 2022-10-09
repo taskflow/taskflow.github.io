7fad2412f831db9e07639bf6f01d61eb4b71e208
Tsung-Wei Huang <twh760812@gmail.com>
2022-10-09 09:55:18 -0500

    updated news

--- js/news.js
+++ js/news.js
@@ -7,16 +7,6 @@ const news = [
     footer: "Last updated on 2022/10/01"
   },
   {
-    title: "Taskflow v3.3 released",
-    image_src: "images/release.png",
-    image_url: "https://taskflow.github.io/taskflow/Releases.html",
-    content: `We have release Taskflow v3.3! This release has sanitized out the data-race issues and introduced a new pipeline programming model. Please check out the <a href="https://taskflow.github.io/taskflow/release-3-3-0.html">release notes</a>.`,
-    footer: "Last updated on 2022/01/03"
-  },
-  {
-    title: "cudaFlow accepted by IEEE TPDS",
-    image_src: "images/tpds.png",
-  {
     title: "Taskflow @ ICPP",
     image_src: "images/icpp-logo.png",
     image_url: "https://icpp22.gitlabpages.inria.fr/prog",
