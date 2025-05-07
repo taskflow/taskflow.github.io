99d1907ad13a7858aad0d9b957d0ca1f3b809679
Tsung-Wei Huang <twhuang@vpn-pa-coop-ptr-216-159.engr.wisc.edu>
2025-05-07 13:51:28 -0500

    update news

--- js/news.js
+++ js/news.js
@@ -3,7 +3,7 @@ const news = [
     title: "Taskflow v3.10 released",
     image_src: "images/release.png",
     image_url: "https://taskflow.github.io/taskflow/Releases.html",
-    content: `We have release Taskflow v3.10! Please check out the <a href="https://taskflow.github.io/taskflow/release-3-10-0.html">release notes</a>. Happy new year to all users!`,
+    content: `We have release Taskflow v3.10! Please check out the <a href="https://taskflow.github.io/taskflow/release-3-10-0.html">release notes</a>.`,
     footer: "Last updated on 2025/05/01"
   },
   {
